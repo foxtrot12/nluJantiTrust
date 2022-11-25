9cb40d788889ee7d9bd00c806f758f9cde8f2ad4
foxtrot12 <43786672+foxtrot12@users.noreply.github.com>
2022-11-25 10:01:29 +0530

    Add files via upload

---
+++ archives.js
@@ -0,0 +1,8 @@
+// for (var i = 0; i < 312; i=i+3) {
+// 	var imgUrl = ("assets/archives/archive"+i+".jpg");
+// 	var picNode = document.createElement('img');
+// 	picNode.src=imgUrl;
+// 	picNode.classList.add("archiveImg");
+
+// 	document.getElementById("archGal").appendChild(picNode);
+// }
