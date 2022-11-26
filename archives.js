599a8c5e6d37ef019c43a0e7971e21a2eddcc1bb
foxtrot12 <43786672+foxtrot12@users.noreply.github.com>
2022-11-26 17:04:20 +0530

    Delete archives.js

--- archives.js
+++
@@ -1,8 +0,0 @@
-// for (var i = 0; i < 312; i=i+3) {
-// 	var imgUrl = ("assets/archives/archive"+i+".jpg");
-// 	var picNode = document.createElement('img');
-// 	picNode.src=imgUrl;
-// 	picNode.classList.add("archiveImg");
-
-// 	document.getElementById("archGal").appendChild(picNode);
-// }
