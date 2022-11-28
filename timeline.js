d2e0f2a42c50120cac3c65c049faad766ebee68d
foxtrot12 <43786672+foxtrot12@users.noreply.github.com>
2022-11-29 01:34:14 +0530

    timeline check

---
+++ timeline.js
@@ -0,0 +1,16 @@
+$(window).on('scroll', function(){
+    function isScrollIntoView(elem, index) {
+      var docViewTop = $(window).scrollTop();
+      var docViewBottom = docViewTop + $(window).height();
+      var elemTop = $(elem).offset().top;
+      var elemBottom = elemTop + $(window).height()*.5;
+      if(elemBottom <= docViewBottom && elemTop >= docViewTop) {
+        $(elem).addClass('active');
+      }
+      if(!(elemBottom <= docViewBottom)) {
+        $(elem).removeClass('active');
+      }
+    }
+    var timeline = $('.vertical-scrollable-timeline li');
+    Array.from(timeline).forEach(isScrollIntoView);
+  });
